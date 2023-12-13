dcffd8f448a2e1199364d96528ffc9a2cef020b4
Kirill Ivanov <kipwoker@users.noreply.github.com>
2023-12-13 20:07:21 +0100

    DAY 13

--- src/day13.rs
+++ src/day13.rs
@@ -10,53 +10,71 @@ impl Solution for Day13 {
     }
 
     fn solve1(&self, input: String) -> String {
-        let matrices = parse(input.as_str());
-
-        let result: i64 = matrices.iter().map(|matrix| {
-            let mut result = 0usize;
-            if let Some(row_index) = find_mirror_row_index(matrix) {
-                let row = row_index + 1;
-                println!("Row {row}");
-                result += row * 100;
-            }
-            let transposed = transpose(matrix);
-            if let Some(col_index) = find_mirror_row_index(&transposed) {
-                let col = col_index + 1;
-                println!("Col {col}");
-                result += col;
-            }
-            result as i64
-        }).sum();
-
-        result.to_string()
+        solve(input.as_str(), 0)
     }
     fn solve2(&self, input: String) -> String {
-        String::new()
+        solve(input.as_str(), 1)
     }
 }
 
-fn is_equal(a: &Vec<char>, b: &Vec<char>) -> bool {
-    if a.len() != b.len() {
-        return false;
-    }
+fn solve(input: &str, expected_diff: usize) -> String {
+    let matrices = parse(input);
 
+    let result: i64 = matrices.iter().enumerate().map(|(idx, matrix)| {
+        let mut result = 0usize;
+        if let Some(row_index) = find_mirror_row_index(matrix, expected_diff) {
+            let row = row_index + 1;
+            println!("Row {idx}: {row}");
+            result += row * 100;
+        }
+        let transposed = transpose(matrix);
+        if let Some(col_index) = find_mirror_row_index(&transposed, expected_diff) {
+            let col = col_index + 1;
+            println!("Col {idx}: {col}");
+            result += col;
+        }
+        result as i64
+    }).sum();
+
+    result.to_string()
+}
+
+fn count_difference(a: &Vec<char>, b: &Vec<char>) -> usize {
+    let mut result = 0;
     for i in 0..a.len() {
         if a[i] != b[i] {
-            return false;
+            result += 1;
         }
     }
 
-    true
+    result
 }
 
-fn find_mirror_row_index(matrix: &Vec<Vec<char>>) -> Option<usize> {
-    let mut cursor = &matrix[0];
+fn find_mirror_row_index(matrix: &Vec<Vec<char>>, expected_diff: usize) -> Option<usize> {
+    let mut cursor = 0;
+    loop {
+        let (candidate, result) = find_mirror_row_index_from(matrix, cursor, expected_diff);
+        if result.is_some() {
+            return result;
+        }
+
+        if candidate.is_none() {
+            return candidate;
+        }
+
+        cursor = candidate.unwrap() + 1;
+    }
+}
+
+fn find_mirror_row_index_from(matrix: &Vec<Vec<char>>, start: usize, expected_diff: usize) -> (Option<usize>, Option<usize>) {
+    let mut cursor = &matrix[start];
 
     let mut mirror_index = None;
     let n = matrix.len();
-    for i in 1..n {
+    for i in (start + 1)..n {
         let row = &matrix[i];
-        if is_equal(cursor, row) {
+        let diff = count_difference(cursor, row);
+        if diff <= expected_diff {
             mirror_index = Some(i - 1);
             break
         }
@@ -64,19 +82,28 @@ fn find_mirror_row_index(matrix: &Vec<Vec<char>>) -> Option<usize> {
         cursor = row;
     }
 
+    //println!("Candidate {mirror_index:?}");
     if mirror_index == None {
-        return None
+        return (None, None)
     }
 
     let mut l = mirror_index.unwrap();
     let mut r = l + 1;
+    let mut total_diff = 0;
     loop {
-        if !is_equal(&matrix[l], &matrix[r]) {
-            break None
+        let diff = count_difference(&matrix[l], &matrix[r]);
+        //println!("Compare {l} {r} {total_diff} {diff} {expected_diff}");
+        total_diff += diff;
+        if total_diff > expected_diff {
+            break (mirror_index, None)
         }
 
         if l == 0 || r == (n - 1) {
-            break mirror_index
+            if total_diff != expected_diff {
+                break (mirror_index, None)
+            } else {
+                break (mirror_index, mirror_index)
+            }
         }
 
         l -= 1;
