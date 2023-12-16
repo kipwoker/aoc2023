48909654bcdc089d3eac9b8bdb0910bd30193ae1
kipwoker <kipwoker@users.noreply.github.com>
2023-12-16 13:07:29 +0100

    Prettify output

--- src/main.rs
+++ src/main.rs
@@ -33,26 +33,30 @@ fn main() {
     let input_test_path = format!("inputs/{day}.test.txt");
     let input_path = format!("inputs/{day}.txt");
 
-    let input_paths = [input_test_path, input_path];
+    let input_paths = [
+        input_test_path,
+        input_path
+    ];
 
     let solvers: Vec<Box<dyn Fn(String) -> String>> = vec![
         Box::new(|x| solution.solve1(x)),
         Box::new(|x| solution.solve2(x)),
     ];
 
-    println!("===================");
+    println!("============================================");
     for path in input_paths {
         let p = path.clone();
-        println!("Use input: {p}");
+        println!("| > {p}");
         let input_content = fs::read_to_string(path.clone()).expect("File not found");
         for (index, solver) in solvers.iter().enumerate() {
+            println!("| ------------------------------------------");
             let start_time = Instant::now();
             let output = solver(input_content.clone());
             let elapsed_time = start_time.elapsed();
             let number = index + 1;
-            println!("Part {number}: {output}");
-            println!("Execution time: {:.2?}", elapsed_time);
+            println!("| Part {number}: {output}");
+            println!("| Execution time: {:.2?}", elapsed_time);
         }
-        println!("===================");
+        println!("============================================");
     }
 }
