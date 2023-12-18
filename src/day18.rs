85265e677d530c5661cc7f1406ba09b3119f2fa5
kipwoker <kipwoker@users.noreply.github.com>
2023-12-18 19:12:51 +0100

    Day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,8 +1,8 @@
 #![allow(unused_variables)]
 
 use std::cmp::min;
-use std::collections::HashSet;
-use crate::core::{Direction, parse_i32, print_matrix, Solution};
+use std::collections::{HashSet, VecDeque};
+use crate::core::{Direction, parse_i32, Solution};
 use crate::core::Direction::{Down, Left, Right, Up};
 
 pub struct Day18 {}
@@ -16,24 +16,84 @@ impl Solution for Day18 {
         let steps = parse(input.as_str());
         let points = get_points(&steps);
         let matrix = get_matrix(&points);
-        print_matrix(&matrix.data);
+        let mut result = bfs(&matrix);
+        result += points.len();
 
-        String::new()
+        result.to_string()
     }
     fn solve2(&self, input: String) -> String {
-        String::new()
+        let steps = parse(input.as_str());
+        let steps: Vec<DigStep> = steps.iter().map(unwrap).collect();
+        let pp = get_pivot_points(&steps);
+        let s = s_gauss(&pp);
+
+        s.to_string()
     }
 }
 
+#[derive(Debug)]
 struct DigStep {
     direction: Direction,
     count: i32,
     color: String
 }
 
-struct Matrix {
-    start: (usize, usize),
-    data: Vec<Vec<bool>>
+fn unwrap(step: &DigStep) -> DigStep {
+    let color = step.color.trim_start_matches(|x| x == '#');
+    let hex = &color[0..5];
+    let count = i32::from_str_radix(hex, 16).unwrap();
+    let direction = match &color.chars().nth(5).unwrap() {
+        '0' => Right,
+        '1' => Down,
+        '2' => Left,
+        '3' => Up,
+        _ => unreachable!()
+    };
+
+    DigStep {
+        color: step.color.clone(),
+        direction,
+        count
+    }
+}
+
+fn s_gauss(points: &Vec<(i64, i64)>) -> i64 {
+    let n = points.len();
+    let mut sum: i64 = 0;
+    for i in 0..n {
+        let i1 = (i + 1) % n;
+        let xi = points[i].1;
+        let yi = points[i].0;
+        let yi1 = points[i1].0;
+        let xi1 = points[i1].1;
+        sum += xi * yi1;
+        sum -= xi1 * yi;
+        sum += abs(xi - xi1) + abs(yi - yi1);
+    }
+
+    sum = abs(sum.clone());
+    sum /= 2;
+
+    sum + 1
+}
+
+fn abs(sum: i64) -> i64 {
+    if sum < 0 {-sum} else {sum}
+}
+
+fn get_pivot_points(steps: &Vec<DigStep>) -> Vec<(i64, i64)> {
+    let mut result = Vec::new();
+
+    let mut cursor = (0,0);
+    for step in steps {
+        let (dy,dx) = step.direction.get_matrix_offset();
+        let y = cursor.0 + (dy as i64) * (step.count as i64);
+        let x = cursor.1 + (dx as i64) * (step.count as i64);
+        cursor = (y, x);
+        result.push((y + 1, x + 1));
+    }
+
+    result
 }
 
 fn get_points(steps: &Vec<DigStep>) -> HashSet<(i32, i32)> {
@@ -54,7 +114,7 @@ fn get_points(steps: &Vec<DigStep>) -> HashSet<(i32, i32)> {
     result
 }
 
-fn get_matrix(points: &HashSet<(i32, i32)>) -> Matrix {
+fn get_matrix(points: &HashSet<(i32, i32)>) -> Vec<Vec<char>> {
     let mut max_y = i32::MIN;
     let mut max_x = i32::MIN;
     let mut min_y = i32::MAX;
@@ -78,19 +138,86 @@ fn get_matrix(points: &HashSet<(i32, i32)>) -> Matrix {
     max_x -= shift;
     max_y -= shift;
 
-    let mut result = vec![vec![false; max_x as usize]; max_y as usize];
+    let mut result = vec![vec!['.'; (max_x + 1) as usize]; (max_y + 1) as usize];
     for point in points {
         let y = (point.0 - shift) as usize;
         let x = (point.1 - shift) as usize;
-        result[y][x] = true;
+        result[y][x] = '#';
     }
 
-    Matrix{
-        start: (-shift as usize, -shift as usize),
-        data: result
-    }
+    result
 }
 
+fn find_pivot_point(matrix: &Vec<Vec<char>>) -> (usize, usize) {
+    let n = matrix.len();
+    let m = matrix[0].len();
+    for i in 0..n {
+        if matrix[i][0] == '#' && matrix[i][1] == '.' {
+            return (i, 1);
+        }
+        if matrix[i][m - 1] == '#' && matrix[i][m - 2] == '.' {
+            return (i, m - 2);
+        }
+    }
+
+    for j in 0..m {
+        if matrix[0][j] == '#' && matrix[1][j] == '.' {
+            return (1, j);
+        }
+        if matrix[n - 1][j] == '#' && matrix[n - 2][j] == '.' {
+            return (n - 2, j);
+        }
+    }
+
+    unreachable!()
+}
+
+fn bfs(matrix: &Vec<Vec<char>>) -> usize {
+    let start = find_pivot_point(matrix);
+    let mut q = VecDeque::new();
+    let mut visited = HashSet::new();
+    q.push_back(start);
+
+    while let Some(point) = q.pop_front() {
+        if visited.contains(&point) {
+            continue;
+        }
+
+        let neighbors = get_neighbors_4(matrix, &point);
+        for p in neighbors {
+            if matrix[p.0][p.1] == '#' {
+                continue;
+            }
+
+            q.push_back(p);
+            visited.insert(point);
+        }
+    }
+
+    visited.len()
+}
+
+fn get_neighbors_4(matrix: &Vec<Vec<char>>, point: &(usize, usize)) -> Vec<(usize, usize)> {
+    let mut result = Vec::new();
+
+    let n = matrix.len();
+    let m = matrix[0].len();
+
+    if point.0 > 0 {
+        result.push((point.0 - 1, point.1));
+    }
+    if point.1 > 0 {
+        result.push((point.0, point.1 - 1));
+    }
+    if point.0 < (n - 1) {
+        result.push((point.0 + 1, point.1));
+    }
+    if point.1 < (m - 1) {
+        result.push((point.0, point.1 + 1));
+    }
+
+    result
+}
 
 fn parse(input: &str) -> Vec<DigStep> {
     input.split("\n").map(|line| {
