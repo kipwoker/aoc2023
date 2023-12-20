e16208b3b119ae92dea8b9268d513631cee055f5
kipwoker <kipwoker@users.noreply.github.com>
2023-12-20 18:45:57 +0100

    Day 20. 2

--- src/day20.rs
+++ src/day20.rs
@@ -1,7 +1,7 @@
 #![allow(unused_variables)]
 
-use std::collections::HashMap;
-use crate::core::Solution;
+use std::collections::{HashMap, HashSet};
+use crate::core::{lcm_of_vector, Solution};
 use crate::day20::ModuleType::{Broadcast, Conjunction, FlipFlop};
 use crate::day20::Pulse::{High, Low};
 
@@ -14,10 +14,7 @@ impl Solution for Day20 {
 
     fn solve1(&self, input: String) -> String {
         let mut module_map = parse(input.as_str());
-        let input_node_map = get_conjunction_input_nodes(&module_map);
         let link = &mut module_map;
-        fill_conjunction_input(link, &input_node_map);
-        //println!("{module_map:?}");
 
         let mut pulse_map = HashMap::from([
             (0, 0),
@@ -37,13 +34,75 @@ impl Solution for Day20 {
         result.to_string()
     }
     fn solve2(&self, input: String) -> String {
-        String::new()
+        let mut module_map = parse(input.as_str());
+
+        let last = "rx".to_string();
+        let (last_conjunction, last_conjunction_module) = module_map.iter().find(|(name, module)| module.output.contains(&last)).unwrap();
+        let last_conjunction = last_conjunction.clone();
+        let last_conjunction = last_conjunction.as_str();
+        let mut end_modules = match &last_conjunction_module.module_type {
+            Conjunction(input_map) => {
+                input_map.keys().map(|x| x.clone()).collect::<HashSet<String>>()
+            }
+            _ => { unreachable!() }
+        };
+
+        println!("{end_modules:?}");
+
+        let mut i = 1;
+        let mut caught_iterations = HashSet::new();
+        loop {
+            send2(&mut module_map, &mut caught_iterations, &i, &mut end_modules, last_conjunction,"mock_value", "broadcaster", Low);
+
+            if end_modules.is_empty() {
+                let vec: Vec<i64> = caught_iterations.iter().map(|x| x.clone() as i64).collect();
+                let total = lcm_of_vector(&vec).unwrap();
+                break total.to_string()
+            }
+
+            i += 1;
+        }
+    }
+}
+
+fn send2(
+    module_map: &mut HashMap<String, Module>,
+    caught_iterations: &mut HashSet<u64>,
+    iteration: &u64,
+    end_modules: &mut HashSet<String>,
+    last_conjunction: &str,
+    sender: &str,
+    receiver: &str,
+    pulse: Pulse,
+) {
+    if let High = pulse {
+        if receiver == last_conjunction && end_modules.contains(sender) {
+            let mul = if iteration & 1 == 0 { iteration + 1 } else { iteration.clone() };
+            println!("High {sender} {receiver} on {iteration} -> {mul}");
+            caught_iterations.insert(mul);
+            end_modules.remove(sender);
+        }
+    }
+
+    if let Some(module) = module_map.get_mut(receiver) {
+        let next = module.send_signal(sender, pulse);
+        for (next_module_name, next_pulse) in next {
+            send2(
+                module_map,
+                caught_iterations,
+                iteration,
+                end_modules,
+                last_conjunction,
+                receiver,
+                next_module_name.as_str(),
+                next_pulse,
+            );
+        }
     }
 }
 
 fn send(module_map: &mut HashMap<String, Module>, pulse_map: &mut HashMap<i32, i32>, sender: &str, receiver: &str, pulse: Pulse) {
-    //println!("Send {pulse:?} to {receiver}");
-    pulse_map.entry(if let Low = pulse {0} else {1}).and_modify(|v| *v = *v + 1);
+    pulse_map.entry(if let Low = pulse { 0 } else { 1 }).and_modify(|v| *v = *v + 1);
     if let Some(module) = module_map.get_mut(receiver) {
         let next = module.send_signal(sender, pulse);
         for (next_module_name, next_pulse) in next {
@@ -55,24 +114,21 @@ fn send(module_map: &mut HashMap<String, Module>, pulse_map: &mut HashMap<i32, i
 #[derive(Debug, Clone)]
 enum Pulse {
     Low,
-    High
+    High,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum ModuleType {
-    // % on/off
     FlipFlop(bool),
-    // & recent_input_pulse_map
     Conjunction(HashMap<String, Pulse>),
-    // "broadcaster"
-    Broadcast
+    Broadcast,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Module {
     name: String,
     output: Vec<String>,
-    module_type: ModuleType
+    module_type: ModuleType,
 }
 
 impl Module {
@@ -81,7 +137,7 @@ impl Module {
             FlipFlop(state) => {
                 if let Low = pulse {
                     *state = !*state;
-                    let new_pulse = if *state {High} else {Low};
+                    let new_pulse = if *state { High } else { Low };
                     self.output.iter().map(|x| (x.clone(), new_pulse.clone())).collect()
                 } else {
                     Vec::new()
@@ -89,8 +145,8 @@ impl Module {
             }
             Conjunction(state) => {
                 state.entry(sender.to_string()).and_modify(|x| *x = pulse);
-                let all_high = state.values().all(|x| if let High = x {true} else {false});
-                let new_pulse = if all_high {Low} else {High};
+                let all_high = state.values().all(|x| if let High = x { true } else { false });
+                let new_pulse = if all_high { Low } else { High };
                 self.output.iter().map(|x| (x.clone(), new_pulse.clone())).collect()
             }
             Broadcast => {
@@ -101,6 +157,14 @@ impl Module {
 }
 
 fn parse(input: &str) -> HashMap<String, Module> {
+    let mut module_map = parse_init(input);
+    let input_node_map = get_conjunction_input_nodes(&module_map);
+    let link = &mut module_map;
+    fill_conjunction_input(link, &input_node_map);
+    module_map
+}
+
+fn parse_init(input: &str) -> HashMap<String, Module> {
     input.split("\n").map(|line| {
         let parts = line.split(" -> ").collect::<Vec<&str>>();
         let header = parts[0];
@@ -114,7 +178,7 @@ fn parse(input: &str) -> HashMap<String, Module> {
                 Module {
                     name,
                     output,
-                    module_type: FlipFlop(false)
+                    module_type: FlipFlop(false),
                 }
             }
             &'&' => {
@@ -122,7 +186,7 @@ fn parse(input: &str) -> HashMap<String, Module> {
                 Module {
                     name,
                     output,
-                    module_type: Conjunction(HashMap::new())
+                    module_type: Conjunction(HashMap::new()),
                 }
             }
             &'b' => {
@@ -130,10 +194,10 @@ fn parse(input: &str) -> HashMap<String, Module> {
                 Module {
                     name,
                     output,
-                    module_type: Broadcast
+                    module_type: Broadcast,
                 }
             }
-            &_ => {unreachable!()}
+            &_ => { unreachable!() }
         };
 
         (module.name.clone(), module)
