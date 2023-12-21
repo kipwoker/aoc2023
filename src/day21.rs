1eb54acbfffd7a8762b8be7d6e7d05ba78ce02ef
kipwoker <kipwoker@users.noreply.github.com>
2023-12-22 00:27:33 +0100

    Moar logs

--- src/day21.rs
+++ src/day21.rs
@@ -43,7 +43,7 @@ impl Solution for Day21 {
         let start = matrix.find(|(y,x)| matrix[*y][*x] == 'S').unwrap();
         matrix[start.0][start.1] = '.';
 
-        let n = matrix.len();
+        let n = matrix.len() as i32;
 
         let mut cursor = HashSet::new();
         cursor.insert((start.0 as i32, start.1 as i32));
@@ -51,18 +51,22 @@ impl Solution for Day21 {
         let target = 26501365;
         let mut nums = Vec::new();
         let mut flag = false;
-        let mut half = 0i32;
+        let mut x1 = 0i32;
 
         let mut prev_sum = 0;
         let mut i = 0i32;
+
+        let direction = 1;
         let mut c=  0;
         let points_limit = 2;
         loop {
             let sum = cursor.len();
 
             if flag {
-                if abs(i) as usize % n == abs(half) as usize {
+                if abs(i) % n == abs(x1) {
                     nums.push(sum);
+                    let x = nums.len();
+                    println!("x = {x} -> i = {i} -> y = {sum}");
                 }
             }
 
@@ -75,13 +79,16 @@ impl Solution for Day21 {
             for point in cursor {
                 let next = matrix.get_neighbors_4_infinity(&point);
                 for p in next {
-                    if !flag && (p.0 < 0 || p.0 >= n as i32 || p.1 < 0 || p.1 >= n as i32) {
-                        half = i;
+                    if !flag && (p.0 < 0 || p.0 >= n || p.1 < 0 || p.1 >= n) {
+                        let prev = i - direction;
+                        println!("x = 0 -> i = {prev} -> y = {prev_sum}");
+                        println!("x = 1 -> i = {i} -> y = {sum}");
+
+                        x1 = i;
+                        c = prev_sum;
+
                         nums.push(sum);
                         flag = true;
-                        c = prev_sum;
-                        println!("Half {half}");
-                        println!("C {c}");
                     }
 
                     let y = modulo(&p.0, &n);
@@ -93,17 +100,13 @@ impl Solution for Day21 {
             }
 
             cursor = next_q;
-            i -= 1;
+            i += direction;
             prev_sum = sum;
         }
 
         println!("{nums:?}");
 
-        let nums: Vec<i32> = nums.iter().map(|x| x.clone() as i32).collect();
-        let y: [i32; 2] = match nums.as_slice() {
-            [a, b] => [*a, *b],
-            _ => unreachable!(),
-        };
+        let y: Vec<i32> = nums.iter().map(|x| x.clone() as i32).collect();
         let x = &[1, 2];
 
         let x02 = x[0] * x[0];
@@ -114,7 +117,7 @@ impl Solution for Day21 {
 
         println!("{a} x^2 + {b} x + {c}");
 
-        let x = ((target - half) / n as i32 + 1) as i64;
+        let x = ((target - x1) / n + 1) as i64;
         let a = a as i64;
         let b = b as i64;
         let c = c as i64;
@@ -124,9 +127,9 @@ impl Solution for Day21 {
     }
 }
 
-fn modulo(a: &i32, b: &usize) -> usize {
-    let bb = *b as i32;
-    let c = *a % (*b as i32);
+fn modulo(a: &i32, b: &i32) -> usize {
+    let bb = *b;
+    let c = *a % bb;
     if c >= 0 {
         c as usize
     } else {
